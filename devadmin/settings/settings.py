2622d3dda4b1d3652cfa76efeaba091eda72d064
micaaell <micaeldantas133@gmail.com>
2025-05-18 14:14:16 -0300

    Atualizações

--- devadmin/settings/settings.py
+++ devadmin/settings/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d&fj#(my1kmiq5l@x10gmtm50hj^5qqc$-m_!o)%&tr*tmslb1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','habitus-2025.onrender.com']
+ALLOWED_HOSTS = ['habitus-2025.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
