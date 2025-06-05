f3788e88c1dc43f3cd7d52f151e85b73f3d7614a
Micael Dantas <110204915+micaaell@users.noreply.github.com>
2025-06-05 18:43:44 +0000

    Deploy inicial

--- devadmin/settings/settings.py
+++ devadmin/settings/settings.py
@@ -77,12 +77,12 @@ ASGI_APPLICATION = 'devadmin.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
 }
+
 
 
 # Password validation
