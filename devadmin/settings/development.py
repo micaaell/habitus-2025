a5d6dc505e207946be41f70b16972b24c113b1df
Micael Dantas <110204915+micaaell@users.noreply.github.com>
2025-06-06 18:42:35 +0000

    add o ALLOWED_HOSTS = ['*']

--- devadmin/settings/development.py
+++ devadmin/settings/development.py
@@ -3,7 +3,7 @@ import os
 DEBUG = True
 #Crie secret key para seu ambiente de desenvolvimento
 SECRET_KEY='ixb62ha#ts=ab4t2u%p1_62-!5w2j==j6d^3-j$!z(@*m+-h'
-ALLOWED_HOSTS = ['habitus-2025.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','habitus-2025.onrender.com','habitus-cnat.vercel.app',]
 DATABASES={
 'default':{
 'ENGINE':'django.db.backends.sqlite3',
--- devadmin/wsgi.py
+++ devadmin/wsgi.py
@@ -11,7 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devadmin.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devadmin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devadmin.settings.development')
 
 application = get_wsgi_application()
 app = application
