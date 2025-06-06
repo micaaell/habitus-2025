e5e364de3436058b224ee69538b07117d3ed2e57
Micael Dantas <110204915+micaaell@users.noreply.github.com>
2025-06-06 02:40:43 +0000

    add o habitus-cnat.vercel.app

--- devadmin/wsgi.py
+++ devadmin/wsgi.py
@@ -1,6 +1,17 @@
+"""
+WSGI config for devadmin project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devadmin.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devadmin.settings.development')
 
 application = get_wsgi_application()
+app = application
